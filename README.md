6e1ff6e2e663e2c010e096c358927d401716adfe
Dylaris <3445818679@qq.com>
2025-08-29 23:31:33 +0800

    give up windows support for raylib

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # pong-game
+
+- **love2d**
+
+```console
+$ cd love2d
+$ love .
+```
+
+- **raylib**
+
+```console
+$ cd raylib
+$ ./build.sh
+$ ./main
+```
--- raylib/build.sh
+++ raylib/build.sh
@@ -2,7 +2,8 @@
 
 set -xe
 
-gcc -Wall -Wextra -o main main.c            \
+gcc -Wall -Wextra                           \
     -I./raylib-5.5_linux_amd64/include      \
+    -o main main.c                          \
     -L./raylib-5.5_linux_amd64/lib -lraylib \
     -Wl,-rpath=./raylib-5.5_linux_amd64/lib
